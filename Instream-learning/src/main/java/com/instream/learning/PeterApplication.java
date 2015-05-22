f21611a736d7a69a48d3c2651e42959671d9a40b
urza710 <urza710@github.com>
2015-05-22 15:42:09 -0400

    import config

---
+++ Instream-learning/src/main/java/com/instream/learning/PeterApplication.java
@@ -0,0 +1,12 @@
+package com.instream.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeterApplication.class, args);
+    }
+}
